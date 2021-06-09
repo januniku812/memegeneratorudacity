77abaf494d59bd56e8c3682439fffe3c09d0ae50
januniku812 <64760311+januniku812@users.noreply.github.com>
2021-06-09 14:17:04 -0500

    Update MemeEngine.py

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -1,59 +1,45 @@
-import argparse
-import os
 import random
-
-from MemeEngine import MemeEngine
-from QuoteEngine import QuoteModel, Ingestor
-
-
-def generate_meme(path=None, body=None, author=None):
-    """ Generate a meme given an path and a quote """
-    img = None
-    quote = None
-
-    if path is None:
-        images = "./_data/photos/dog/"
-        imgs = []
-        for root, dirs, files in os.walk(images):
-            imgs = [os.path.join(root, name) for name in files]
-
-        img = random.choice(imgs)
-    else:
-        img = path[0]
-
-    if body is None:
-        quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
-                       './_data/DogQuotes/DogQuotesDOCX.docx',
-                       './_data/DogQuotes/DogQuotesPDF.pdf',
-                       './_data/DogQuotes/DogQuotesCSV.csv']
-        quotes = []
-        for f in quote_files:
-            quotes.append(Ingestor.Ingestor.parse(f))
-            print(quotes)
-        quotes_list_one = quotes[random.randint(0, 3)]
-        quote = random.choice(quotes_list_one)
-        print(quote)
-    else:
-        if author is None:
-            raise Exception('Author Required if Body is Used')
-        quote = QuoteModel(body, author)
-
-    meme = MemeEngine('./static')
-    path = meme.make_meme(img, quote.body, quote.author)
-    return path
+from PIL import ImageDraw, ImageFont
+import PIL.Image
 
 
-if __name__ == "__main__":
-    # path - path to an image file
-    # body - quote body to add to the image
-    # author - quote author to add to the image
-    parser = argparse.ArgumentParser(description="Welcome to Meme Generator.")
-    parser.add_argument('--path', type=str,
-                        default=None, help="path to an image file")
-    parser.add_argument('--body', type=str,
-                        default=None, help="quote body to add to the image")
-    parser.add_argument('--author', type=str,
-                        default=None, help="quote author to add to the image")
+class MemeEngine:
+    def __init__(self, output_path):
+        """Create instance of class"""
+        self.output_path = output_path
 
-    args = parser.parse_args()
-    print(generate_meme(args.path, args.body, args.author))
+    def make_meme(self, path, text, author, width=500):
+        print("make meme reached")
+        """Create meme with given text and author"""
+        global img, img_font, height, img_draw
+        try:
+            img = PIL.Image.open(path)
+        except Exception as ex:
+            print(f'Exception: {ex}')
+
+        if width:
+            ratio = width / float(img.size[0])
+            height = int(ratio * float(img.size[1]))
+            size = (width, height)
+            img = img.resize(size, PIL.Image.NEAREST)
+
+        line_to_draw = f'{text}\n- {author}'
+
+        if text:
+            img_draw = ImageDraw.Draw(img)
+            fnt_size = int(height/(len(line_to_draw.strip(" ")))) * 2
+            fnt_path = './fonts/LilitaOne-Regular.ttf'
+            img_font = ImageFont.truetype(fnt_path, int(height/11))
+
+        # Calculate y axis for text display
+        y_min = (img.size[1] / 20)
+        y_max = img.size[1]
+        range_y = random.randint(y_min, y_max)
+
+        # draw text
+        img_draw.multiline_text((0, range_y), line_to_draw, 'white', img_font)
+
+        out_path = f'{self.output_path}/{random.randint(0, 1000000)}.jpeg'
+        img.save(out_path)
+        print(f"meme saved to {self.output_path}")
+        return out_path
